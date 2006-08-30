bb1fda13c569ab199c971cfaefe545512a870026
Kirsten Jones <synedra@gmail.com>
2006-08-30 05:03:36 +0000

    Import of SYNEDRA/Socialtext-Resting-0.01 from CPAN.
    gitpan-cpan-distribution: Socialtext-Resting
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         SYNEDRA/Socialtext-Resting-0.01.tar.gz
    gitpan-cpan-author:       SYNEDRA
    gitpan-cpan-maturity:     released

---
+++ lib/Socialtext/Resting.pm
@@ -0,0 +1,414 @@
+package Socialtext::Resting;
+
+use strict;
+use warnings;
+
+use URI::Escape;
+use LWP::UserAgent;
+use HTTP::Request;
+use Class::Field 'field';
+
+use Readonly;
+
+our $VERSION = '0.01';
+
+=head1 NAME
+
+Socialtext::Resting - module for accessing Socialtext REST APIs
+
+=head1 SYNOPSIS
+
+  use Socialtext::Resting;
+  my $Rester = Socialtext::Resting->new(
+    username => $opts{username},
+    password => $opts{password},
+    server   => $opts{server},
+  );
+  $Rester->workspace('wikiname');
+  $Rester->get_page('my_page');
+}
+
+=head1 DESCRIPTION
+
+C<Socialtext::Resting> is a module designed to allow remote access
+to the Socialtext REST APIs for use in perl programs.
+
+=head1 METHODS
+
+=cut
+
+Readonly my $BASE_URI => '/data/workspaces';
+Readonly my %ROUTES   => (
+    page           => $BASE_URI . '/:ws/pages/:pname',
+    pages          => $BASE_URI . '/:ws/pages',
+    pagetag        => $BASE_URI . '/:ws/pages/:pname/tags/:tag',
+    pagetags       => $BASE_URI . '/:ws/pages/:pname/tags',
+    pageattachment => $BASE_URI
+        . '/:ws/pages/:pname/attachments/:attachment_id',
+    pageattachments      => $BASE_URI . '/:ws/pages/:pname/attachments',
+    workspace            => $BASE_URI . '/:ws',
+    workspaces           => $BASE_URI,
+    workspacetag         => $BASE_URI . '/:ws/tags/:tag',
+    workspacetags        => $BASE_URI . '/:ws/tags',
+    workspaceattachment  => $BASE_URI . '/:ws/attachments/:attachment_id',
+    workspaceattachments => $BASE_URI . '/:ws/attachments',
+    workspaceuser        => $BASE_URI . '/:ws/users/:user_id',
+    workspaceusers       => $BASE_URI . '/:ws/users',
+    user                 => '/data/users/:user_id',
+    users                => '/data/users',
+);
+
+field 'workspace';
+field 'username';
+field 'password';
+field 'server';
+
+=head2 new
+
+    my $Rester = Socialtext::Resting->new(
+        username => $opts{username},
+        password => $opts{password},
+        server   => $opts{server},
+    );
+    
+    Creates a Socialtext::Resting object for the 
+    specified server/user/password combination.
+
+=cut
+
+sub new {
+    my $invocant = shift;
+    my $class    = ref($invocant) || $invocant;
+    my $self     = {@_};
+    return bless $self, $class;
+}
+
+=head2 get_page 
+
+    $Rester->workspace('wikiname');
+    $Rester->get_page('page_name');
+
+    Retrieves the content of the specified page.  Note that 
+    the workspace method needs to be called first to specify 
+    which workspace to operate on.
+
+=cut
+
+sub get_page {
+    my $self = shift;
+    my $pname = shift;
+
+    my $uri = $self->_make_uri(
+        'page',
+        { pname => $pname, ws => $self->workspace }
+    );
+
+    my ( $status, $content ) = $self->_request(
+        uri    => $uri,
+        method => 'GET',
+        accept => 'text/x.socialtext-wiki',
+    );
+
+    if ( $status == 200 || $status == 404 ) {
+        return $content;
+    }
+    else {
+        die "$status: $content\n";
+    }
+}
+
+=head2 get_attachment
+
+    $Rester->workspace('wikiname');
+    $Rester->get_attachment('attachment_id);
+
+    Retrieves the specified attachment from the workspace.  
+    Note that the workspace method needs to be called first 
+    to specify which workspace to operate on.
+
+=cut
+
+# REVIEW: dup with above, some
+sub get_attachment {
+    my $self          = shift;
+    my $attachment_id = shift;
+
+    my $uri = $self->_make_uri(
+        'workspaceattachment',
+        { attachment_id => $attachment_id, ws => $self->workspace, }
+    );
+
+    my ( $status, $content ) = $self->_request(
+        uri    => $uri,
+        method => 'GET',
+    );
+
+    if ( $status == 200 || $status == 404 ) {
+        return $content;
+    }
+    else {
+        die "$status: $content\n";
+    }
+}
+
+=head2 put_pagetag
+
+    $Rester->workspace('wikiname');
+    $Rester->put_pagetag('page_name', 'tag');
+
+    Add the specified tag to the page.
+
+=cut
+
+sub put_pagetag {
+    my $self  = shift;
+    my $pname = shift;
+    my $tag   = shift;
+
+    my $uri = $self->_make_uri(
+        'pagetag',
+        { pname => $pname, ws => $self->workspace, tag => $tag }
+    );
+
+    my ( $status, $content ) = $self->_request(
+        uri    => $uri,
+        method => 'PUT',
+    );
+
+    if ( $status == 204 || $status == 201 ) {
+        return $content;
+    }
+    else {
+        die "$status: $content\n";
+    }
+}
+
+=head2 delete_pagetag
+
+    $Rester->workspace('wikiname');
+    $Rester->delete_pagetag('page_name', 'tag');
+
+    Delete the specified tag from the page.
+
+=cut
+
+sub delete_pagetag {
+    my $self  = shift;
+    my $pname = shift;
+    my $tag   = shift;
+
+    my $uri = $self->_make_uri(
+        'pagetag',
+        { pname => $pname, ws => $self->workspace, tag => $tag }
+    );
+
+    my ( $status, $content ) = $self->_request(
+        uri    => $uri,
+        method => 'DELETE',
+    );
+
+    if ( $status == 204 ) {
+        return $content;
+    }
+    else {
+        die "$status: $content\n";
+    }
+}
+
+=head2 post_attachment
+
+    $Rester->workspace('wikiname');
+    $Rester->post_attachment('page_name',$id,$content,$mime_type);
+
+    Attach the file to the specified page
+
+=cut
+
+sub post_attachment {
+    my $self               = shift;
+    my $pname              = shift;
+    my $attachment_id      = shift;
+    my $attachment_content = shift;
+    my $attachment_type    = shift;
+
+    my $uri = $self->_make_uri(
+        'pageattachments',
+        {
+            pname => $pname,
+            ws    => $self->workspace
+        },
+    );
+
+    $uri .= "?name=$attachment_id";
+
+    my ( $status, $content, $location ) = $self->_request(
+        uri     => $uri,
+        method  => 'POST',
+        type    => $attachment_type,
+        content => $attachment_content,
+    );
+
+    $location =~ m{.*/attachments/([^/]+)};
+    $location = URI::Escape::uri_unescape($1);
+
+    if ( $status == 204 || $status == 201 ) {
+        return $location;
+    }
+    else {
+        die "$status: $content\n";
+    }
+}
+
+=head2 put_page 
+
+    $Rester->workspace('wikiname');
+    $Rester->put_page('page_name',$content);
+
+    Save the content as a page in the wiki.
+
+=cut
+
+sub put_page {
+    my $self         = shift;
+    my $pname        = shift;
+    my $page_content = shift;
+
+    my $uri = $self->_make_uri(
+        'page',
+        { pname => $pname, ws => $self->workspace }
+    );
+
+    my ( $status, $content ) = $self->_request(
+        uri     => $uri,
+        method  => 'PUT',
+        type    => 'text/x.socialtext-wiki',
+        content => $page_content,
+    );
+
+    if ( $status == 204 || $status == 201 ) {
+        return $content;
+    }
+    else {
+        die "$status: $content\n";
+    }
+}
+
+sub _make_uri {
+    my $self         = shift;
+    my $thing        = shift;
+    my $replacements = shift;
+
+    my $uri = $ROUTES{$thing};
+
+    # REVIEW: tried to do this in on /g go but had issues where
+    # syntax errors were happening...
+    foreach my $stub ( keys(%$replacements) ) {
+        my $replacement
+            = URI::Escape::uri_escape_utf8( $replacements->{$stub} );
+        $uri =~ s{/:$stub\b}{/$replacement};
+    }
+
+    return $uri;
+}
+
+=head2 get_pages 
+
+    $Rester->workspace('wikiname');
+    $Rester->get_pages();
+
+    List all pages in the wiki.
+
+=cut
+
+sub get_pages {
+    my $self = shift;
+
+    return $self->_get_things('pages');
+}
+
+sub _get_things {
+    my $self         = shift;
+    my $things       = shift;
+    my %replacements = @_;
+
+    my $uri = $self->_make_uri(
+        $things,
+        { ws => $self->workspace, %replacements }
+    );
+
+    my ( $status, $content ) = $self->_request(
+        uri    => $uri,
+        method => 'GET',
+        accept => 'text/plain',
+    );
+
+    if ( $status == 200 ) {
+        return ( grep defined, ( split "\n", $content ) );
+    }
+    elsif ( $status == 404 ) {
+        return ();
+    }
+    else {
+        die "$status: $content\n";
+    }
+}
+
+sub get_pagetag {
+}
+
+=head2 get_pagetags
+
+    $Rester->workspace('wikiname');
+    $Rester->get_pagetags('page_name');
+
+    List all pagetags on the specified page 
+
+=cut
+
+sub get_pagetags {
+    my $self  = shift;
+    my $pname = shift;
+
+    return $self->_get_things( 'pagetags', pname => $pname );
+}
+
+=head2 get_workspaces
+
+    $Rester->get_workspaces();
+
+    List all workspaces on the server
+
+=cut
+
+sub get_workspaces {
+    my $self = shift;
+
+    return $self->_get_things('workspaces');
+}
+
+sub _request {
+    my $self = shift;
+    my %p    = @_;
+    my $ua   = LWP::UserAgent->new();
+    my $uri  = $self->server . $p{uri};
+    warn "uri: $uri\n";
+    my $request = HTTP::Request->new( $p{method}, $uri );
+    $request->authorization_basic( $self->username, $self->password );
+
+    $request->header( 'Accept'       => $p{accept} ) if $p{accept};
+    $request->header( 'Content-Type' => $p{type} )   if $p{type};
+    $request->content( $p{content} ) if $p{content};
+
+    my $response = $ua->simple_request($request);
+
+    my $location = $response->header('location');
+
+    return ( $response->code, $response->content, $location );
+}
+
+=head1 AUTHORS
+
+Chris Dent, C<< <chris.dent@socialtext.com> >>
+Kirsten Jones C<< <kirsten.jones@socialtext.com> >>
+
+1;
---
+++ t/resting.t
@@ -0,0 +1,63 @@
+use Test::More;
+use Socialtext::Resting;
+use IPC::Run;
+
+use strict;
+use warnings;
+
+plan tests => 4;
+
+my $Strutter = Socialtext::Resting->new(
+        username => 'rest-tester@socialtext.com',
+        password => 'dozing',
+        server   => 'http://www.socialtext.net',
+);
+
+$Strutter->workspace('st-rest-test');
+
+# Put the page
+my $page_content = "This is a\nfile thing here\n";
+eval { $Strutter->put_page("Test page", $page_content);};
+my $network_skip = 1 if $@;
+
+SKIP: {
+    skip "unable to access test server", 4 if $network_skip;
+    # Get it back and check it
+    my $content = $Strutter->get_page("Test page");
+
+    like ($content, qr/file thing here/, 
+            'Content has both lines');
+
+    # Put 2 attachments
+    my $text_content = readfile("t/filename.txt");
+    my $jpg_content  = readfile("t/file.jpg");
+    my $text_id = $Strutter->post_attachment(
+            "Test page", "filename.txt", $text_content, "text/plain");
+    my $jpeg_id = $Strutter->post_attachment(
+            "Test page", "file.jpg", $jpg_content, "image/jpeg");
+
+    my $retrieved_text = $Strutter->get_attachment($text_id);
+    my $retrieved_jpeg = $Strutter->get_attachment($jpeg_id);
+    is ($text_content, $retrieved_text, "text attachment roundtrips");
+    is ($jpg_content, $retrieved_jpeg, "jpeg attachment roundtrips");
+
+    # Set a tag or two
+    $Strutter->put_pagetag("Test page", "Taggy");
+    $Strutter->put_pagetag("Test page", "Taggity tag");
+    my $tags = join (' ', $Strutter->get_pagetags("Test page"));
+
+    like( $tags, qr/Taggity tag/, "Tag with spaces included");
+}
+
+sub readfile {
+    my ($filename) = shift;
+    if (! open (NEWFILE, $filename)) {
+        print STDERR "$filename could not be opened for reading: $!\n";
+        return;
+    }
+    local $/;
+    my $data = <NEWFILE>;
+    close (NEWFILE);
+
+    return ($data);
+}
